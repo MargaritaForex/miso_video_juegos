bf50138fdba517a4296d7992ac5ac160d26e7e49
milindr4123 <e.foreros@uniandes.edu.co>
2025-04-27 06:55:46 -0500

    componente

--- src/ecs/systems/s_animation.py
+++ src/ecs/systems/s_animation.py
@@ -3,23 +3,21 @@ from src.ecs.components.c_animation import CAnimation
 from src.ecs.components.c_surface import CSurface
 
 
-def system_animation(world: esper.World, delta_time: CAnimation):
+def system_animation(world:esper.World, delta_time:float):
     components = world.get_components(CSurface, CAnimation)
 
     for _, (c_s, c_a) in components:
-        # 1. Disminuir el valor actual del tiempo de la animación
+        # 1. Dimsinuir el valor de curr_time de la nimacion
         c_a.curr_anim_time -= delta_time
-
-        # 2. Si el tiempo es menor o igual a 0, se hace cambio de frame
+        # 2. Cuando curr_imte <= 0
         if c_a.curr_anim_time <= 0:
-            # 2.1 Reiniciar el tiempo
-            c_a.curr_anim_time = c_a.animations_list[c_a.curr_anim].framerate
-            # 3. Cambiar el frame
-            c_a.curr_anim_time += 1
-
-            # 4. Limitar el frame actual al rango de la animación
+            # CAMBIO DE FRAME
+            c_a.curr_frame += 1
+            # Limitar el frame con sus propiedad de start y end
+            # Limitar el frame con sus propiedad de start y end
             if c_a.curr_frame > c_a.animations_list[c_a.curr_anim].end:
                 c_a.curr_frame = c_a.animations_list[c_a.curr_anim].start
+            # Calcular la nueva sub anea del rectangulo de sprite
                 # Calcular la nueva subtarea del rectangulo de sprite
             rect_surf = c_s.surf.get_rect()
             c_s.area.w = rect_surf.w / c_a.number_frames
